bcd162d9bc1795d7430c5a343375d70290d83390
Fonteny Noa <o22305140@or-iut-i002-f03.iut45.univ-orleans.fr>
2025-09-01 15:27:45 +0200

    j'ai tout fait mais ça marche pas je sais pas pourquoi

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+def home(request,param):
+    return HttpResponse(f"<h1>Bonjour { param }</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Nous contater</h1><p>07 69 01 88 61</p>") 
+
+def about(request):
+    return HttpResponse("<h1>A propos de nous</h1><p>Je suis Noa</p>")

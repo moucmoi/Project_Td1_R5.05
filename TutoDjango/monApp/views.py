b81b6bd373a5c01bcf6024570a43b5ec99a685d9
Compte Temporaire INFO 3 <temporaire-info3@or-iut-i102-f12.iut45.univ-orleans.fr>
2025-09-04 11:27:55 +0200

    param marche

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request,param):
-    return HttpResponse(f"<h1>Bonjour { param }</h1>")
+def home(request,param=None):
+    if param is None:
+        return HttpResponse("<h1>Bonjour inconu</h1>")
+    else:
+        return HttpResponse("<h1>Bonjour "+param+"</h1>")
 
 def contact(request):
     return HttpResponse("<h1>Nous contater</h1><p>07 69 01 88 61</p>") 

57e30977d4a8fa0a1e46eb1523aedd0ae8a96a0e
Compte Temporaire INFO 3 <temporaire-info3@or-iut-i102-f12.iut45.univ-orleans.fr>
2025-09-04 12:07:03 +0200

    migration 004-status

--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -30,9 +30,17 @@ class Rayon(models.Model):
     def __str__(self):
             return self.nomRayon
 
+
 class Contenir(models.Model):
     qte = models.DecimalField(max_digits=10, decimal_places=0)
     idP = models.ForeignKey(Produit, on_delete=models.CASCADE,related_name="produits",null=False,blank=True)
     idR = models.ForeignKey(Rayon, on_delete=models.CASCADE,related_name="rayon",null=False,blank=True)
 
+
+class Status(models.Model):
+    idStat = models.AutoField(primary_key=True)
+    libelle = models.CharField(max_length=100)
+
+    def __str__(self):
+            return self.libelle
 

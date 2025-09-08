c7d238daf6546c4f606bf7daf01c7a66e260ba04
Fonteny Noa <noatoimoi@gmail.com>
2025-09-08 15:27:39 +0200

    fin séance 3

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Produit
+from .models import *
 
-# Register your models here.
-admin.site.register(Produit)
+class ProduitAdmin(admin.ModelAdmin):
+    model = Produit
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "status"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabProd"]
+    radio_fields = {"status": admin.VERTICAL}
+    search_fields = ('intituleProd', 'dateFabProd')
+    list_filter = ('status', 'dateFabProd')
+
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
+admin.site.register(Produit, ProduitAdmin)
+admin.site.register(Categorie, CategorieAdmin)
+admin.site.register(Statut)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -10,21 +10,21 @@ class Categorie(models.Model):
         return self.nomCat
 
 class Statut(models.Model):
-    idStatus = models.AutoField(primary_key=True)
-    libelleStatus = models.CharField(max_length=100)
+    idStatut = models.AutoField(primary_key=True)
+    libelleStatut = models.CharField(max_length=100)
 
     def __str__(self):
-            return self.libelleStatus
+            return self.libelleStatut
 
 class Produit(models.Model):
     refProd = models.AutoField(primary_key=True)
     intituleProd = models.CharField(max_length=200)
     prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
-    dateFabrication =models.DateField(default=date.today)
+    dateFabProd =models.DateField(default=date.today)
 
     # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie 1,1 côté produit)→
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
-    idStatus = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="Statut",null=True, blank=True)
+    status = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="Statut",null=True, blank=True)
     
     def __str__(self):
         return self.intituleProd
@@ -44,5 +44,9 @@ class Contenir(models.Model):
     idR = models.ForeignKey(Rayon, on_delete=models.CASCADE,related_name="rayon",null=False,blank=True)
 
 
+    class Meta:
+        unique_together = ('idP', 'idR')  # Un produit dans un rayon spécifique est unique
 
+    def __str__(self):
+        return f"{self.idP} dans {self.idR} (Qte: {self.qte})"
 
